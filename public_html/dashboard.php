a3fba484bc595a97101b65a6dbbd93d9f0b34f06
nayananga@acerubuntu18.04 <nayanangamuhandiram@gmail.com>
2018-12-14 00:26:17 +0530

    purchase management section implementing

--- public_html/dashboard.php
+++ public_html/dashboard.php
@@ -43,13 +43,13 @@ if (!isset($_SESSION["userid"])) {
 							<iframe src="http://free.timeanddate.com/clock/i616j2aa/n1993/szw160/szh160/cf100/hnce1ead6" frameborder="0" width="160" height="160"></iframe>
 						</div>
 						<div class="col-sm-6">
-							<div class="card">
+							<!-- <div class="card">
 						      <div class="card-body">
-						        <h4 class="card-title">New Orders</h4> <!-- Use this part foit calculationr System user and deb -->
+						        <h4 class="card-title">New Orders</h4> 
 						        <p class="card-text">Here you can make invoices and create new orders</p>
 						        <a href="new_order.php" class="btn btn-primary">New Orders</a>
 						      </div>
-						    </div>
+						    </div> -->
 						</div>
 					</div>
 				</div>
@@ -85,7 +85,7 @@ if (!isset($_SESSION["userid"])) {
 						<div class="card-body">
 						<h4 class="card-title">Purchases</h4> <!-- Brands -->
 						<p class="card-text">Here you can manage your Purchases and you can add new Purchases.</p>
-						<a href="#" data-toggle="modal" data-target="#form_brand" class="btn btn-primary">Add</a>
+						<a href="new_purchase.php" class="btn btn-primary">Add</a>
 						<a href="manage_brand.php" class="btn btn-primary">Manage</a>
 					</div>
 				</div>
